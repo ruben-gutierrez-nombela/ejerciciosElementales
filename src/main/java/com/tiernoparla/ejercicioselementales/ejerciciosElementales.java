cafa8e1ab3c03406495909935366328223adbfc1
ruben-gutierrez-nombela <ruben.gutierreznombela@tiernoparla.es>
2022-09-27 18:51:24 +0200

    añado src/

---
+++ src/main/java/com/tiernoparla/ejercicioselementales/ejerciciosElementales.java
@@ -0,0 +1,11 @@
+package com.tiernoparla.ejercicioselementales;
+
+public class ejerciciosElementales {
+    
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    } // main
+
+
+
+} //ejerciciosElementales
