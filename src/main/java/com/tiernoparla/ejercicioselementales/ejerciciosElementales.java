734798c377276d683bb68893030b121ba41f8f54
ruben-gutierrez-nombela <ruben.gutierreznombela@tiernoparla.es>
2022-09-27 20:09:46 +0200

    terminado ejercicio print pares version 1 - for por pares

--- src/main/java/com/tiernoparla/ejercicioselementales/ejerciciosElementales.java
+++ src/main/java/com/tiernoparla/ejercicioselementales/ejerciciosElementales.java
@@ -3,9 +3,15 @@ package com.tiernoparla.ejercicioselementales;
 public class ejerciciosElementales {
     
     public static void main(String[] args) {
-        System.out.println("Hello World");
-    } // main
-
-
-
+        // printear nums pares del 1 al 10
+        for (int i = 0; i < 12; i = i+2) {
+            System.out.println(i);
+        } //for
+    
+    
+    
+    
+    
+    
+    }//main
 } //ejerciciosElementales
