6c9c5dca5017253358eca5d743681666d17222ce
ruben-gutierrez-nombela <ruben.gutierreznombela@tiernoparla.es>
2022-09-28 17:54:40 +0200

    src/main/java/com/tiernoparla/ejercicioselementales/ejerciciosElementales.java

--- src/main/java/com/tiernoparla/ejercicioselementales/ejerciciosElementales.java
+++ src/main/java/com/tiernoparla/ejercicioselementales/ejerciciosElementales.java
@@ -1,17 +1,32 @@
 package com.tiernoparla.ejercicioselementales;
 
 public class ejerciciosElementales {
-    
+
     public static void main(String[] args) {
+        
         // printear nums pares del 1 al 10
+        
+        for(int i = 1;i < 12; i++) {
+            
+            if(i%2 == 0){
+                System.out.println(i);
+            }
+        }
+        
+        /*
+        
         for (int i = 0; i < 12; i = i+2) {
             System.out.println(i);
         } //for
-    
-    
-    
-    
-    
-    
-    }//main
+         
+        int a = 33;
+        int b = 2;
+        
+        System.out.println(a%b);
+        
+        if (false) {
+            System.out.println("era true");
+        } // if
+        */
+    } //main
 } //ejerciciosElementales
